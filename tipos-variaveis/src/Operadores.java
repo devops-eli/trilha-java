107a368a9f880c66c1f308c7ca31f6eb5d3ac56b
Elias <eliasgustavo849@msn.com>
2025-04-02 01:55:45 -0300

    up

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int a,b;
+    a= 12;
+    b =12;
+    String resultado = a == b?"verdadeiro":"falso";
+    System.out.println(resultado);
+  }
+}
